e1f0b4a102a5776c5e05254456c365061602c07a
Donrington <carryoby@gmail.com>
2025-03-23 00:49:18 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from main import app
+from flask_socketio import SocketIO
+
+socketio = SocketIO(app)
 
 if __name__ == "__main__":
-    app.run()
+    # This is okay for local testing only
+    socketio.run(app, debug=False)
